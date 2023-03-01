06b1bfdc9f16158d6508781b249f0c142de18d62
NullMa <86356074+NulMa@users.noreply.github.com>
2023-03-01 10:39:19 +0900

    .

--- Assets/scripts/Main_char.cs
+++ Assets/scripts/Main_char.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Main_char : MonoBehaviour
 {
+
+    public GameObject MSound;
+    public Rigidbody2D rigid;
+
     // Start is called before the first frame update
     void Start()
     {
